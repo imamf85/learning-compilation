00d38528858c82b778c3a548698b916fc91600d2
KKDewi <khantikdewi@gmail.com>
2022-11-23 00:12:04 +0700

    feat(): Update routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/authorization/Login";
+import Dashboard from "./components/homepage/dashboard/Dashboard";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
