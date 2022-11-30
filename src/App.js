e3e162ab0c09559f1cc9725b446197839c3edcc9
KKDewi <khantikdewi@gmail.com>
2022-12-01 01:43:32 +0700

    feat():Code the card of carlist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/authorization/Login";
+import CarList from "./components/homepage/carList/CarList";
 import Dashboard from "./components/homepage/dashboard/Dashboard";
+
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/car-list" element={<CarList />} />
       </Routes>
     </div>
   );
