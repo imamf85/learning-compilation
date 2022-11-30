e439958277f6e1a8cfe1538af4dadeba0aa438bc
Imam Fachrudin <imamfachrudin9@gmail.com>
2022-11-30 22:43:53 +0700

    feat(dashboard): new sidebar component (left side)

--- src/atom/Sidebar.js
+++ src/atom/Sidebar.js
@@ -1,27 +1,43 @@
-import { Col, Nav, Navbar } from "react-bootstrap";
-import './dashboard.css';
-import homeIcon from '../assest/fi_home.svg';
-import truck from '../assest/fi_truck.svg'
-import Sidebar2 from "./Sidebar2";
+import { Col, Nav, Row, Navbar } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
+import homeIcon from "../assest/fi_home.svg";
+import truck from "../assest/fi_truck.svg";
+import "./atom.css";
 
 const Sidebar = () => {
-    return ( 
-       <Col>
-       <Navbar className="dashboard-nav">
+  const dataNameOnDashboard = ["Dashboard", "Cars", "List Car"];
+  let location = useLocation();
+  return (
+    <Col>
+      <Col className="dashboard-nav">
         <Nav className="brand"></Nav>
-        <Nav.Link>
+        <Nav.Link as={Link} to={{ pathname: "/dashboard" }}>
           <img src={homeIcon} alt="car-pic" />
-          <Col>Dashboard</Col>
+          <Col>{dataNameOnDashboard[0]}</Col>
         </Nav.Link>
-        <Nav.Link>
+        <Nav.Link as={Link} to={{ pathname: "/cars" }}>
           <img src={truck} alt="truck-pic" />
-          <Col>Cars</Col>
+          <Col>{dataNameOnDashboard[1]}</Col>
         </Nav.Link>
-       </Navbar>
-       <Sidebar2 />
-       </Col>
-       
-     );
-}
- 
+      </Col>
+      <Col className="dashboard-nav-child">
+        <Row></Row>
+        <Navbar>
+          <Nav>
+            {dataNameOnDashboard[location.pathname === "/dashboard" ? 0 : 1]}
+          </Nav>
+          <Nav.Link
+            as={Link}
+            to={{
+              pathname: location.pathname,
+            }}
+          >
+            {dataNameOnDashboard[location.pathname === "/dashboard" ? 0 : 2]}
+          </Nav.Link>
+        </Navbar>
+      </Col>
+    </Col>
+  );
+};
+
 export default Sidebar;
