def7d9aa68c9504120a838fd22d102f7d1296b84
Imam Fachrudin <imamfachrudin9@gmail.com>
2022-11-30 22:43:21 +0700

    adjustment update for App and Index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import Sidebar2 from "./atom/Sidebar2";
+import CarsGraph from "./atom/CarsGraph";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./molecules/Dashboard";
+import Cars from "./molecules/Cars";
+import Sidebar from "./atom/Sidebar";
+import TopBar from "./atom/TopBar";
 
 const App = () => {
   return (
-  
-  <Sidebar2 />
-
+    <>
+      <Sidebar />
+      <TopBar />
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cars" element={<Cars />} />
+      </Routes>
+    </>
   );
 };
 
