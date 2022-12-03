039d217e60c252ce94c8c05c04c461653923a4fd
patarebenezer <patarstar42@yahoo.co.id>
2022-12-03 10:10:46 +0700

    feat(login, getCars): Add private route and refactoring getCars

--- src/molecules/CarList.js
+++ src/molecules/CarList.js
@@ -1,19 +1,34 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { Card, Col, Container } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 import { NumberFormat } from "../atom/NumberFormat";
-import NotFoundImg from "../assets/ImageNotFound.jpeg";
+import { fetchCars } from "../config/api";
+
+import Button from "react-bootstrap/Button";
+import Sidebar from "../atom/Sidebar";
+import TopBar from "../atom/TopBar";
+
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import DriveFileRenameOutlineOutlinedIcon from "@mui/icons-material/DriveFileRenameOutlineOutlined";
+import NotFoundImg from "../assets/ImageNotFound.jpeg";
 import "../style.css";
-import { useNavigate } from "react-router-dom";
 
 const CarList = () => {
   const navigate = useNavigate();
-  const token = localStorage.getItem("Token");
-  const config = {
-    headers: { access_token: token, "Content-Type": "application/json" },
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getDataCars();
+  }, []);
+
+  const getDataCars = () => {
+    fetchCars()
+      .then((res) => setData(res.data.cars))
+      .catch((err) => console.error(err));
+  };
+
+  const editCars = (id) => {
+    navigate(`/edit/${id}`);
   };
 
   const styles = {
@@ -28,53 +43,41 @@ const CarList = () => {
       margin: "1rem",
     },
   };
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = () => {
-    axios
-      .get("https://bootcamp-rent-cars.herokuapp.com/admin/v2/car", config)
-      .then((res) => setData(res.data.cars))
-      .catch((err) => console.log(err));
-  };
-
-  const Edit = (id) => {
-    navigate(`/edit${id}`);
-  };
 
   return (
-    <Container style={{ marginTop: 150, marginLeft: 350 }}>
-      <Col className="grid-cars-list">
-        {data.map((items) => (
-          <Col key={items.id}>
-            <Card style={styles.card}>
-              <Card.Img style={styles.cardImage} variant="top" src={items.image ? items.image : NotFoundImg} />
-              <Card.Body>
-                <Card.Text>{items.name}</Card.Text>
-                <Card.Title>Rp. {NumberFormat(items?.price)} / hari</Card.Title>
-                <Card.Text>{items.category}</Card.Text>
-                <Card.Text>{items.updateAt}</Card.Text>
-                <div className="d-flex justify-content-center gap-3">
-                  <Button variant="success" size="lg">
-                    <DriveFileRenameOutlineOutlinedIcon sx={{ fontSize: 25 }} />
-                    Edit
-                  </Button>
-                  <div className="d-grid" onClick={() => Edit(items.id)}>
-                    <Button variant="outline-danger" size="lg">
-                      <DeleteOutlineOutlinedIcon sx={{ fontSize: 25 }} />
-                      Delete
+    <>
+      <Sidebar />
+      <TopBar />
+      <Container style={{ marginTop: 150, marginLeft: 350 }}>
+        <Col className="grid-cars-list">
+          {data.map((items) => (
+            <Col key={items.id}>
+              <Card style={styles.card}>
+                <Card.Img style={styles.cardImage} variant="top" src={items.image ? items.image : NotFoundImg} />
+                <Card.Body>
+                  <Card.Text>{items.name}</Card.Text>
+                  <Card.Title>Rp. {NumberFormat(items?.price)} / hari</Card.Title>
+                  <Card.Text>{items.category}</Card.Text>
+                  <Card.Text>{items.updateAt}</Card.Text>
+                  <div className="d-flex justify-content-center gap-3">
+                    <Button variant="success" size="lg">
+                      <DriveFileRenameOutlineOutlinedIcon sx={{ fontSize: 25 }} />
+                      Edit
                     </Button>
+                    <div className="d-grid" onClick={() => editCars(items.id)}>
+                      <Button variant="outline-danger" size="lg">
+                        <DeleteOutlineOutlinedIcon sx={{ fontSize: 25 }} />
+                        Delete
+                      </Button>
+                    </div>
                   </div>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Col>
-    </Container>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Col>
+      </Container>
+    </>
   );
 };
 
--- src/molecules/Dashboard.js
+++ src/molecules/Dashboard.js
@@ -1,11 +1,15 @@
 import { Col, Container, Breadcrumb, Row } from "react-bootstrap";
 import CarsGraph from "../atom/CarsGraph";
 import Index from "../atom/cars_table/Index";
+import Sidebar from "../atom/Sidebar";
+import TopBar from "../atom/TopBar";
 import "../style.css";
 
 const Dashboard = () => {
   return (
     <>
+    <Sidebar />
+    <TopBar />
       <Col className="dashboard">
         <Col className="empty-area"></Col>
         <Container style={{ width: "80vw", marginTop: 32 }}>
