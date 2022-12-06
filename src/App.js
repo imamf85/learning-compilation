2400b22e89d73a48018d5c4173a076acdf143471
KKDewi <khantikdewi@gmail.com>
2022-12-07 00:05:59 +0700

    feat(): refactoring app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { Route, Routes } from "react-router-dom";
-import CarList from "./molecules/CarList";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./molecules/Dashboard";
-import EditList from "./molecules/EditList";
+import CarList from "./molecules/CarList";
 import Login from "./components/authorization/Login";
 import PrivateRoutes from "./molecules/PrivateRoutes";
 import AddCar from "./molecules/AddCar";
 
 const App = () => {
   return (
-    <>
+    <div>
       <Routes>
         <Route element={<PrivateRoutes />}>
-          <Route element={<Dashboard />} path="/" exact />
+          <Route element={<Dashboard />} path="/dashboard" exact />
           <Route element={<CarList />} path="/car-list" />
-          <Route element={<EditList />} path="/car-list/edit/:id" />
+          <Route element={<AddCar />} path="/addcar" />
         </Route>
         <Route element={<Login />} path="/login" />
+        <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
-    </>
+    </div>
   );
 };
 
